91a87cb60b2c7cb0f9657bf85925b2e36e5b1f5e
Yuge Zhang <Yuge.Zhang@microsoft.com>
2025-08-06 16:44:09 +0800

    Pin torch version (#29)

--- scripts/setup_latest_gpu.sh
+++ scripts/setup_latest_gpu.sh
@@ -4,7 +4,7 @@ python -m pip install --upgrade --no-cache-dir pip
 
 pip install --no-cache-dir packaging ninja numpy pandas ipython ipykernel gdown wheel setuptools
 # This has to be pinned for VLLM to work.
-pip install --no-cache-dir torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
+pip install --no-cache-dir torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu128
 pip install --no-cache-dir flash-attn --no-build-isolation
 pip install --no-cache-dir vllm
 
