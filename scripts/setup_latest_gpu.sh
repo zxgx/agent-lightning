5724f63cfc75bcc2f4fb56958ef384d307717c18
Yuge Zhang <Yuge.Zhang@microsoft.com>
2025-09-12 19:14:53 -0700

    Bump agentops, pin verl (#90)

--- scripts/setup_latest_gpu.sh
+++ scripts/setup_latest_gpu.sh
@@ -6,12 +6,10 @@ pip install --no-cache-dir packaging ninja numpy pandas ipython ipykernel gdown 
 # This has to be pinned for VLLM to work.
 pip install --no-cache-dir torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu128
 pip install --no-cache-dir flash-attn --no-build-isolation
-pip install --no-cache-dir vllm
-
-git clone https://github.com/volcengine/verl
-cd verl
-pip install --no-cache-dir -e .
-cd ..
+# This must match pytorch version.
+pip install --no-cache-dir vllm==0.10.1.1
+# Latest VERL release version.
+pip install --no-cache-dir verl
 
 pip install --no-cache-dir -e .[dev,agent]
 # Upgrade agentops to the latest version
