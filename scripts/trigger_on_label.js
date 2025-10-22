34437dd6f52e16623945d922fc6c9ea4f519848c
Yuge Zhang <Yuge.Zhang@microsoft.com>
2025-10-21 20:29:29 -0700

    Trigger the workflows on certain labels and reopen event (#190)

---
+++ scripts/trigger_on_label.js
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+module.exports = function triggerOnLabel({ core, context, labelName }) {
+  if (!labelName) {
+    throw new Error("labelName is required");
+  }
+
+  if (context.eventName !== "pull_request") {
+    core.setOutput("should-run", "true");
+    core.notice("Triggering this workflow because event is not a pull request");
+    return;
+  }
+
+  core.notice(`This workflow is triggered by ${context.eventName}`);
+
+  const labels = (context.payload.pull_request?.labels ?? []).map(
+    (label) => label.name
+  );
+  core.notice(`Pull request labels: ${labels.join(", ")}`);
+
+  if (labels.includes(labelName)) {
+    core.setOutput("should-run", "true");
+    core.notice(
+      `Triggering this workflow because pull request has the '${labelName}' label.`
+    );
+  } else {
+    core.setOutput("should-run", "false");
+    core.notice(
+      `Skipping because pull request is missing the '${labelName}' label.`
+    );
+  }
+};
